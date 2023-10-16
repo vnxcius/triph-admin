8676a9c73d0a958abc788608b3fee76f7b77d3ed
Vini <vsgh-gouveia@hotmail.com>
2023-10-16 10:31:30 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
